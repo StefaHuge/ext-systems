d2348e27c5f206b1447ff351555b065877663d78
Stefa <bolshavikova1994@gmail.com>
2026-03-01 12:19:58 +0900

    All in end lessons

---
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -0,0 +1,23 @@
+package edu.javacourse.register.dao;
+
+import edu.javacourse.register.domain.Person;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class PersonDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Person> findPersons() {
+        Query query = entityManager.createNamedQuery("Person.findPersons");
+        return query.getResultList();
+    }
+
+    public Long addPerson(Person person) {
+            entityManager.persist(person);
+            entityManager.flush();
+        return person.getPersonId();
+    }
+}
